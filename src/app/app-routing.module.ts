b0f47ed4eea134af4a9fc5e36476e96d89b4df88
GeronimoSchmidt <88343992+GeronimoSchmidt@users.noreply.github.com>
2021-10-04 14:42:06 -0300

    git commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,10 @@ const routes: Routes = [
     path: 'inicio',
     loadChildren: () => import('./inicio/inicio.module').then( m => m.InicioPageModule)
   },
+  {
+    path: 'recetas',
+    loadChildren: () => import('./recetas/recetas.module').then( m => m.RecetasPageModule)
+  },
 
 ];
 
