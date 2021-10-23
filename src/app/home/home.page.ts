0071f9ca367b23a067ef0a5e8467c1215c4ec71a
PanchoTeruggi <BRAVO32f@gmail.com>
2021-10-23 15:24:08 -0300

    commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
 img:string;
+img2:string;
+img3:string;
   constructor() { }
 
   ngOnInit() {
     this.img = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSPdEVNJLFqTW79T_MahATxbmPLxzdOAc8P4Q&usqp=CAU.jpg'
-  }
+    this.img2 = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXTBVqI7BBWFapyfwLQ-jS3mdMuEgwJQxfQw&usqp=CAU"
+    this.img3 = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEC0mkmSsKGrxUYWZRQcN-pAStxXJBgl2dEA&usqp=CAU"
 
+  }
 }
