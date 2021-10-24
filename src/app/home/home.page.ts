ae71bcdf2870e896b040f6ca854ce0ea73c3d45c
PanchoTeruggi <BRAVO32f@gmail.com>
2021-10-24 18:02:25 -0300

    commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -12,7 +12,7 @@ img3:string;
   constructor() { }
 
   ngOnInit() {
-    this.img = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSPdEVNJLFqTW79T_MahATxbmPLxzdOAc8P4Q&usqp=CAU.jpg'
+    this.img = 'https://www.foodtempel.de/wp-content/uploads/2017/12/rotes-Pesto-mit-getrockneten-Tomaten-Pesto-rosso-768x576.jpg'
     this.img2 = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXTBVqI7BBWFapyfwLQ-jS3mdMuEgwJQxfQw&usqp=CAU"
     this.img3 = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEC0mkmSsKGrxUYWZRQcN-pAStxXJBgl2dEA&usqp=CAU"
 
